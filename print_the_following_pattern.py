4cd7589258ccc76fdd7f0f89933b8ebe875789b5
sravyareddy6735 <84090201+sravyareddy6735@users.noreply.github.com>
2022-11-07 23:06:22 +0530

    n=int(input()) for i in range(n):     l=['0']*n     l[i]=l[(len(l)-1)-(i)]='x'     l=''.join(l)     print(l)

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,10 +1,6 @@
-def pattern(n):
-    for i in range(n):
-        for j in range(n):
-            if(j==0 or j==n-1) or (i==j):
-                print("*",end=" ")
-            else:
-                print(" ",end=" ")
-        print()
-s=int(input())
-pattern(s)
+n=int(input())
+for i in range(n):
+    l=['0']*n
+    l[i]=l[(len(l)-1)-(i)]='x'
+    l=''.join(l)
+    print(l)
